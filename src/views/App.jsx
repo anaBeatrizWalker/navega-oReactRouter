55976fa72886d85244a966bd86640b30bd2a5e73
Ana Beatriz <anabiawalker@gmail.com>
2021-12-20 15:04:13 -0300

    Estruturando o projeto

---
+++ src/views/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import React from "react";
+
+const App = props => {
+    <div className="app">
+        Componente App
+    </div>
+}
+
+export default App
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = props => {
+    <div className="home">
+        <h1>Home</h1>
+        <p>Bem-vindo</p>
+    </div>
+}
+
+export default Home
